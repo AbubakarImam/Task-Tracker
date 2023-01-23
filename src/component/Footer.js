495e79f3002e17a6fa16b7c86a27adc3979932e7
AbubakarImam <salamabubakarimam@gmail.com>
2023-01-23 18:50:22 +0100

    initial commit

---
+++ src/component/Footer.js
@@ -0,0 +1,12 @@
+
+
+const Footer = () => {
+  return (
+    <footer className='mx-auto flex flex-col justify-center items-center'>
+        <p>Copyright &copy; 2023</p>
+        <a href='/about'>About</a>
+    </footer>
+  )
+}
+
+export default Footer
